216f1b37a4ee22ecc5e5f3cc246b474866925f1f
romanized <ytspace1@gmail.com>
2026-02-12 16:56:22 +0100

    Toevoegen van Monster.cs
    - Monster.cs bestand is aangemaakt

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62613c5b115005e1a8e645f998e6e5ddf017535e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
