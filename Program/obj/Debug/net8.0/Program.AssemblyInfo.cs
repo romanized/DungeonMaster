d140c42613473f0e13b7e9722f43f9276ca5543c
ShivrajVs <1105109@hr.nl>
2026-03-03 13:52:19 +0100

    Weopon class made

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200209dd68dc1c451413ad466b48c81b128dc508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fd73120b4e1cead48a565b85032c3152b65e63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
