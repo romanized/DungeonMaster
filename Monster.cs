28937d9f226967c39cf074632018462af4b1995d
Joshua Mohanlal <mohanlaljoshua@gmail.com>
2026-03-03 14:18:25 +0100

    Implement Monster class with properties and constructor

--- Monster.cs
+++ Monster.cs
@@ -1,1 +1,17 @@
-#test
+public class Monster
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int MaxHealth { get; set; }
+    public int CurrentHealth { get; set; }
+
+    public Monster(int id, string name, int damage, int maxHealth, int currentHealth)
+    {
+        ID = id;
+        Name = name; 
+        Damage = damage;
+        MaxHealth = maxHealth;
+        CurrentHealth = currentHealth;
+    }
+}
